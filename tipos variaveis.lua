891ddb6aa550ca4301ff71225f6dbb33c263158f
ClaudioABittencourt <byttencourt@hotmail.com>
2025-04-10 20:50:11 -0300

    v1

---
+++ tipos variaveis.lua
@@ -0,0 +1,35 @@
+local monster = "creeper" -- Strings
+local health = 20 -- numeric
+local ishungry = true -- boolean
+
+print("_______________________________________________")
+print(monster, health, ishungry)
+print(type(monster))
+print(type(health))
+print(type(ishungry))
+
+print("Tipos de variaveis são Strings(palavras), numeros ou boolean(verdadeiro ou falso)")
+print("Variáveis constantes são aquelas que tem informações definidas e não podem ser modificadas depois de reinicializados os programas")
+
+local monster <const> = "Zombie"
+
+print(monster)
+
+-- Estilo de codigo
+
+local mais_ou_menos_assim = 1 -- snake_case
+local isAngry = 1 -- camelCase
+local MonsterName = 1 -- PascalCase
+local MONSTER_NAME = 1 -- SCREAMING_SNAKE_CASE
+
+print(MONSTER_NAME)
+
+-- Comentarios curtos
+
+--[[ 
+    Várias linhas
+    para construir 
+    o nosso comentario 
+    em bloco.
+]]
+
---
+++ variaveis.lua
@@ -0,0 +1,11 @@
+print("_________________________________________________")
+print("Váriaveis são nomes no código que contém dados.")
+print("_________________________________________________")
+local name = "NINO"
+local year = 2023
+local add = 5
+local myboolean = true
+
+print(name)
+print(year + add)
+print(myboolean)
