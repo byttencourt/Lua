8f79f61568fee1e18f9b22416dab8bb3032186a4
ClaudioABittencourt <byttencourt@hotmail.com>
2025-04-10 21:04:19 -0300

    Create operadoresAritmeticos.lua

---
+++ operadoresAritmeticos.lua
@@ -0,0 +1,9 @@
+local health = 20
+health = health + 10 -- redefiniu a variavel health para 30
+print(health + 20) -- apenas alterou a variavel health para exibir na print valor com adição de 20
+
+-- operadores aritméticos
+
+local life = 5498.1234
+
+print(life % 1)
