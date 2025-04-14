ee2f0d85e1435be22a5050bf0761abb25566723c
ClaudioABittencourt <byttencourt@hotmail.com>
2025-04-14 12:54:36 -0300

    att

--- Trilha Lua/desafio.lua
+++ Trilha Lua/desafio.lua
@@ -1,9 +1,11 @@
 local MONSTER_NAME = "Creeper"
 local description = "Monstro do Minecraft"
-
+local emoji = "💥"
+-- para funcionar o emoji chcp 65001
+os.execute("chcp 65001")
 
 print("===================================================")
-print("|    ", MONSTER_NAME)
-print("|    ", description)
-print("|")
+print("|  ", MONSTER_NAME)
+print("|  ", description)
+print("|  ", emoji)
 print("===================================================")
