634efd6d1b2ec0abad75cee370b0fa1bd0a26fe5
ClaudioABittencourt <byttencourt@hotmail.com>
2025-04-14 12:42:01 -0300

    att

---
+++ Trilha Lua/desafio.lua
@@ -0,0 +1,9 @@
+local MONSTER_NAME = "Creeper"
+local description = "Monstro do Minecraft"
+
+
+print("===================================================")
+print("|    ", MONSTER_NAME)
+print("|    ", description)
+print("|")
+print("===================================================")
