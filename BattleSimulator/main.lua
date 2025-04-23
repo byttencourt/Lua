32acf0d85f11ce294fa9885732d8b37a7607e332
ClaudioABittencourt <byttencourt@hotmail.com>
2025-04-23 16:28:44 -0300

    att

--- BattleSimulator/main.lua
+++ BattleSimulator/main.lua
@@ -67,6 +67,7 @@ while true do
     --ponto de saida Boss sem vida
     if boss.health <=0 then
         print(string.format("%s venceu o confronto! Nem o %s é paril para ele!", player.name, boss.name))
+        utils.credits()
         break
     end
     -- simular o turno da criatura
@@ -78,6 +79,7 @@ while true do
     --ponto de saida jogador sem vida
     if player.health <= 0 then
         print(string.format("%s Perdeu o confronto! %s parece imbatível!", player.name, boss.name))
+        utils.credits()
         break
     end
 end
